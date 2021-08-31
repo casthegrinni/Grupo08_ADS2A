042a63d1cc08735b41285a3ed67d91b68d5b12ad
Casthegrinni <79323766+casthegrinni@users.noreply.github.com>
2021-08-31 18:54:13 -0300

    Update tryingReading.py

--- Python/tryingReading.py
+++ Python/tryingReading.py
@@ -6,7 +6,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
 		host="localhost",
 		user="root",
-		password="AbCdELEFOA",
+		password="zzz",
 		database="testandoPython"
 		)
 mycursor = mydb.cursor()
