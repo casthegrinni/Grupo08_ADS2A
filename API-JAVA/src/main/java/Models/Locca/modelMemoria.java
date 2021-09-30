eab0fa666baea2487a9fab00f8c535a4d3e74085
phklein <79323655+phklein@users.noreply.github.com>
2021-09-29 21:10:03 -0300

    commit de emergencia

---
+++ API-JAVA/src/main/java/Models/Locca/modelMemoria.java
@@ -0,0 +1,15 @@
+
+package Models.Locca;
+
+public class modelMemoria {
+    private Double uso;
+
+    public Double getUso() {
+        return uso;
+    }
+
+    public void setUso(Double uso) {
+        this.uso = uso;
+    }
+    
+}
