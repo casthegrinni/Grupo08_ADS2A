437dfc6055cb80d04c2bb8a0eb412ea3d4cc25b3
BrunnoCastigrini <brunno.castigrini@bandtec.com.br>
2021-11-02 17:27:11 -0300

    refactor(login-web): passando as funções para um arquivo js

--- Estático/Projeto Site/public/src/js/login.js
+++ Estático/Projeto Site/public/src/js/login.js
@@ -9,3 +9,42 @@ function anterior() {
     form2.style.display = "none";
     progress.style.width = "140px";
 }
+
+function entrar() {
+    aguardar();
+    var formulario = new URLSearchParams(new FormData(form_login));
+    fetch("../usuarios/autenticar", {
+        method: "POST",
+        body: formulario
+    }).then(resposta => {
+        if (resposta.ok) {
+
+            resposta.json().then(json => {
+
+                sessionStorage.login_usuario_meuapp = json.login;
+                sessionStorage.nome_usuario_meuapp = json.nome;
+
+                window.location.href = 'dashboard.html';
+            });
+
+        } else {
+
+            console.log('Erro de login!');
+
+            resposta.text().then(texto => {
+                console.error(texto);
+                finalizar_aguardar(texto);
+            });
+        }
+    });
+
+    return false;
+}
+
+function aguardar() {
+    // btn_entrar.disabled = true;
+}
+
+function finalizar_aguardar(resposta) {
+    // btn_entrar.disabled = false;
+}
