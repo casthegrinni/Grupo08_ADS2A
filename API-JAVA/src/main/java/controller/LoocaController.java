95e6a64f0ecae01ab1a06c4446fc6b23e5dfa694
FelipeAmorim720 <felipe.reis@bandtec.com.br>
2021-11-06 00:58:26 -0300

    feat(API_JAVA): geração de tratamento de alertas e envios de mensagens especializadas ao slack

--- API-JAVA/src/main/java/controller/LoocaController.java
+++ API-JAVA/src/main/java/controller/LoocaController.java
@@ -83,26 +83,39 @@ public class LoocaController {
 
         if (porcentagemDisco >= 71.0 && porcentagemDisco < 81.0) {
             System.out.println("Disco em risco");
-        } else if (porcentagemDisco >= 81.0){
+            slack.initializer();
+            json.put("text", String.format(":warning:? ALERTA LARANJA NO DISCO RÍGIDO, COMPONENTE COM %.1f% EM USO! :warning:", porcentagemDisco));
+            SlackModel.sendMessage(json);
+        } else if (porcentagemDisco >= 81.0) {
             System.out.println("Disco em alerta critico");
+            slack.initializer();
+            json.put("text", String.format(":rotating_light: ALERTA CRÍTICO DO SISTEMA NO DISCO RÍGIDO COM OS NÍVEIS EM %.1f%!  :rotating_light:", porcentagemDisco));
+            SlackModel.sendMessage(json);
         }
-        
-        if (porcentagemMemoria >= 71.0 && porcentagemMemoria < 81.0) {
-            System.out.println("memoria em risco");
-        } else if (porcentagemMemoria >= 81.0){
-            System.out.println("memoria em alerta critico");
-        }
-        
-        if (usoProcessador >= 71.0 && usoProcessador < 81.0) {
-            System.out.println("cpu em risco");
-        } else if (usoProcessador >= 81.0){
-            System.out.println("cpu em alerta critico");
-        }
-        
-        //slack.initializer();
-        //json.put("text", "Bot em testes :)");
-        //SlackModel.sendMessage(json);
 
+        if (porcentagemMemoria >= 71.0 && porcentagemMemoria < 81.0) {
+            System.out.println("Memoria em risco");
+            slack.initializer();
+            json.put("text", String.format(":warning:? ALERTA LARANJA NA MEMÓRIA RAM, COMPONENTE COM %.1f% EM USO! :warning:?", porcentagemMemoria));
+            SlackModel.sendMessage(json);
+        } else if (porcentagemMemoria >= 81.0) {
+            System.out.println("Memoria em alerta critico");
+            slack.initializer();
+            json.put("text", String.format(":rotating_light: ALERTA CRÍTICO DO SISTEMA NA MEMÓRIA RAM COM OS NÍVEIS EM %.1f%! :rotating_light:", porcentagemMemoria));
+            SlackModel.sendMessage(json);
+        }
+
+        if (usoProcessador >= 71.0 && usoProcessador < 81.0) {
+            System.out.println("CPU em risco");
+            slack.initializer();
+            json.put("text", String.format(":warning:? ALERTA LARANJA EM CPU, COMPONENTE COM %.1f% EM USO! :warning:", usoProcessador));
+            SlackModel.sendMessage(json);
+        } else if (usoProcessador >= 81.0) {
+            System.out.println("CPU em alerta critico");
+            slack.initializer();
+            json.put("text", String.format(":rotating_light: ALERTA CRÍTICO DO SISTEMA NA CPU COM OS NÍVEIS EM %.1f%! :rotating_light:", usoProcessador));
+            SlackModel.sendMessage(json);
+        }
     }
 
 }
