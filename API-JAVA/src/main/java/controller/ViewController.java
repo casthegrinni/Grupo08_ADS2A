f2054e92f0b30164d1513c21ef1dfca2346da04a
BrunnoCastigrini <brunno.castigrini@bandtec.com.br>
2021-11-20 14:23:48 -0300

    feat(java-python): o java escreve no arquivo que o python le

--- API-JAVA/src/main/java/controller/ViewController.java
+++ API-JAVA/src/main/java/controller/ViewController.java
@@ -1,6 +1,9 @@
 package controller;
 
 import Models.DataBaseModel;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import java.sql.Array;
 import java.sql.ResultSet;
@@ -10,18 +13,19 @@ import java.util.Map;
 import javax.swing.JOptionPane;
 
 public class ViewController {
-    private final DataBaseModel db= new DataBaseModel();
+
+    private final DataBaseModel db = new DataBaseModel();
     private final LoocaController looca = new LoocaController();
     private final SlackController s = new SlackController();
 
-    public Boolean verifyUserAndMachine(String login, String senha,String fkMaquina) {
+    public Boolean verifyUserAndMachine(String login, String senha, String fkMaquina) throws IOException {
         String query = String.format("select email,senha,fk_estacao from [dbo].[usuario] where email ='%s' and senha = '%s';", login, senha);
         Map map = db.makeSelectQuery(query);
         Integer fkmaquinaInt = 0;
         try {
             fkmaquinaInt = Integer.valueOf(fkMaquina);
             s.setFkMaquina(fkmaquinaInt);
-            
+
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Insira apenas números no id da maquina");
         }
@@ -29,24 +33,38 @@ public class ViewController {
             return false;
         } else {
             Boolean checked = checkFkMaquina(fkmaquinaInt);
-            if ( checked){
+            if (checked) {
+                letterToPython(String.format("%d", fkmaquinaInt)); 
                 return true;
+            } else {
+                return false;
             }
-            else return false;
 
         }
     }
+
     public Boolean checkFkMaquina(int fkMaquina) {
-        String query = String.format("select * from maquina where id_maquina = %d",fkMaquina);
+        String query = String.format("select * from maquina where id_maquina = %d", fkMaquina);
         Map map = db.makeSelectQuery(query);
-        if (map.isEmpty()){
+        if (map.isEmpty()) {
             return false;
+        } else {
+            return true;
         }
-        else return true;
-
 
     }
-    public void start(){
+
+    public void letterToPython(String id)
+            throws IOException {
+        FileWriter file = new FileWriter("../SCREEN-READER-LINUX/dist/machine.ini");
+        PrintWriter writer = new PrintWriter(file);
+        writer.print("[machine_config]");
+        writer.println("");
+        writer.printf("machine_id = %s", id);
+        writer.close();
+    }
+
+    public void start() {
         looca.setSlack(s);
         looca.insertInSeconds(5);
         looca.alertInMinutes();
@@ -57,38 +75,28 @@ public class ViewController {
         db.initializer();
     }
 
-    public boolean calibratePc(Boolean requested, String fkMaquina){
+    public boolean calibratePc(Boolean requested, String fkMaquina) {
         Integer fkInt = 0;
         fkInt = Integer.valueOf(fkMaquina);
         looca.setFkMaquina(fkInt);
 
-
-
-        if (!requested){
+        if (!requested) {
             System.out.println("verificando se o pc já foi checado alguma vez");
 
-           String response =  db.makeCalibrateSelect(fkMaquina);
-           if (response.equals("0") || response.equals("1")){
-               looca.setStaticPcInfo();
-               return true;
-           }
-           else if(response.equals("")){
-               return false;
+            String response = db.makeCalibrateSelect(fkMaquina);
+            if (response.equals("0") || response.equals("1")) {
+                looca.setStaticPcInfo();
+                return true;
+            } else if (response.equals("")) {
+                return false;
 
-           }
-           else {
-               looca.setStaticPcInfo();
-               return true;
-           }
+            } else {
+                looca.setStaticPcInfo();
+                return true;
+            }
         }
         return false;
 
-
-
     }
-
-
-
-
 
 }
