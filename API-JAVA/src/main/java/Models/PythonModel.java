c942d2379d7c7479da5c0358fa8a48bfd997e5ae
BrunnoCastigrini <brunno.castigrini@bandtec.com.br>
2021-11-20 16:07:43 -0300

    feat(python-java): mudei o caminho e os comandos, fazendo ele abrir pelo java

--- API-JAVA/src/main/java/Models/PythonModel.java
+++ API-JAVA/src/main/java/Models/PythonModel.java
@@ -5,41 +5,42 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class PythonModel {
+
     private final String os;
 
     public PythonModel(String os) {
         this.os = os;
     }
-    
+
     public void letterToPython(String id)
             throws IOException {
         System.out.println(os);
         String path;
-        if(os.contains("Windows")){
+        if (os.contains("Windows")) {
             path = "../SCREEN-READER-WINDOWS/dist/machine.ini";
         } else {
             path = "../SCREEN-READER-LINUX/dist/machine.ini";
         }
-        
+
         FileWriter file = new FileWriter(path);
         PrintWriter writer = new PrintWriter(file);
         writer.print("[machine_config]");
         writer.println("");
         writer.printf("machine_id = %s", id);
         writer.close();
-        
+
         openPython();
     }
-    
-    public void openPython () throws IOException{
+
+    public void openPython() throws IOException {
         String command;
-         if(os.contains("Windows")){
+        if (os.contains("Windows")) {
             command = "cd ../SCREEN-READER-WINDOWS/dist && start ScreenReader.exe";
+            final Process exec = new ProcessBuilder("CMD", "/C", command).start();
         } else {
             command = "../SCREEN-READER-LINUX/dist/screenReader";
+            final Process exec = new ProcessBuilder("/bin/bash", "-c", command).start();
         }
-        String[] args = new String[] {"/bin/bash", "-c", "cd ,./"};
-        Process proc = new ProcessBuilder(args).start();
     }
 
 }
