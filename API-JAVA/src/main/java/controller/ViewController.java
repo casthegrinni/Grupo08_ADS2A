9b75c01efa78f7e69ca78ace2a1c516b75445eef
Gabriel-Prisco <gabriel.psilva@bandtec.com.br>
2021-11-24 19:56:14 -0300

    refactor: corrigindo chamadas do java e regra de negócios no python

--- API-JAVA/src/main/java/controller/ViewController.java
+++ API-JAVA/src/main/java/controller/ViewController.java
@@ -24,15 +24,16 @@ public class ViewController {
         String query = String.format("select email,senha,fk_estacao from [dbo].[usuario] where email ='%s' and senha = '%s';", login, senha);
         Map map = db.makeSelectQuery(query);
         Integer fkmaquinaInt = 0;
-        logs.saveLogs("Aplicação iniciada por: "+login);
+        logs.saveLogs("Aplicaï¿½ï¿½o iniciada por: "+login);
 
         try {
             fkmaquinaInt = Integer.valueOf(fkMaquina);
             s.setFkMaquina(fkmaquinaInt);
 
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Insira apenas números no id da maquina");
-            logs.saveLogs("Erro ao iniciar aplicação");
+            JOptionPane.showMessageDialog(null, "Insira apenas nÃºmeros no id da mÃ¡quina");
+            System.out.println("\nInsira apenas nÃºmeros no id da mÃ¡quina");
+            logs.saveLogs("Erro ao iniciar aplicaÃ§Ã£o");
         }
         if (map.isEmpty()) {
             return false;
@@ -76,7 +77,7 @@ public class ViewController {
         looca.setFkMaquina(fkInt);
 
         if (!requested) {
-            System.out.println("verificando se o pc jÃ¡ foi checado alguma vez");
+            System.out.println("\nverificando se o pc jÃ¡ foi checado alguma vez");
 
             String response = db.makeCalibrateSelect(fkMaquina);
             if (response.equals("0") || response.equals("1")) {
